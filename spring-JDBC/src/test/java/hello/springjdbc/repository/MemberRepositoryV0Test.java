fcff6e0589507bc001c343649a2437c7b4886044
gwangho-y <yigwangho21@gmail.com>
2024-06-26 19:59:19 +0900

    조회

--- spring-JDBC/src/test/java/hello/springjdbc/repository/MemberRepositoryV0Test.java
+++ spring-JDBC/src/test/java/hello/springjdbc/repository/MemberRepositoryV0Test.java
@@ -1,10 +1,12 @@
 package hello.springjdbc.repository;
 
 import hello.springjdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -18,13 +20,21 @@ import static org.junit.jupiter.api.Assertions.*;
  * -----------------------------------------------------------
  * 2024-06-24        dlrhk       최초 생성
  */
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV1", 10000);
+        Member member = new Member("memberV3", 10000);
         repository.save(member);
 
+
+        Member findMamber = repository.findById(member.getMemberId());
+        log.info("findmemberid == {}", findMamber);
+        log.info("member == findMember {}", member == findMamber);
+        // Member의 @Data 롬복이 equals메서드를 자동으로 생성해서 값을 비교해 주기 때문에 true가 나온다.
+        log.info("member == findMember {}", member.equals(findMamber));
+        assertThat(findMamber).isEqualTo(member);
     }
 }
