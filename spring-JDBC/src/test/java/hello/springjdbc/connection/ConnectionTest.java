8d07efc159a21584215b5f40895f6a90fff828c9
gwangho-y <yigwangho21@gmail.com>
2024-06-30 14:16:41 +0900

    dataSource 예저1

---
+++ spring-JDBC/src/test/java/hello/springjdbc/connection/ConnectionTest.java
@@ -0,0 +1,50 @@
+package hello.springjdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.springjdbc.connection.ConnectionConst.*;
+
+/**
+ * packageName    : com.kovo.domain.ticketlink
+ * fileName       : ConnectionTest
+ * author         : gwangho
+ * date           : 2024-06-30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-06-30        dlrhk       최초 생성
+ */
+@Slf4j
+public class ConnectionTest {
+    @Test
+    void driverManager() throws SQLException {
+
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection1={}, class={}", con1, con1.getClass());
+        log.info("connection2={}, class={}", con2, con2.getClass());
+
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        userDataSource(dataSource);
+    }
+
+    private void userDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("con1={}, class={}", con1, con1.getClass());
+        log.info("con2={}, class={}", con2, con2.getClass());
+    }
+}
