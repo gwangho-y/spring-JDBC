82bbb3720146c3e5833b073cce7d998f53aabab7
gwangho-y <yigwangho21@gmail.com>
2024-07-09 21:47:59 +0900

    트랜잭션 템플릿 사용에 따른 중복코드 처리

--- spring-JDBC/src/main/java/hello/springjdbc/service/MemberServiceV3_2.java
+++ spring-JDBC/src/main/java/hello/springjdbc/service/MemberServiceV3_2.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.SQLException;
 
@@ -15,46 +16,38 @@ import java.sql.SQLException;
  * fileName       : MemberServiceV1
  * author         : gwangho
  * date           : 2024-07-01
- * description    : 트랜잭션 - 파라메터 연동, 풀을 고려한 종료
+ * description    : 트랜잭션 - 트랜잭션 템플릿
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2024-07-01        dlrhk       최초 생성
  */
-@RequiredArgsConstructor
 @Slf4j
-public class MemberServiceV3_1 {
+public class MemberServiceV3_2 {
 
-//    private final DataSource dataSource;
-    private final PlatformTransactionManager transactionManager;
 
+    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
 
-    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        // 트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
 
-
-        try {
-
-            bizLogic( fromId, toId, money);
-
-            transactionManager.commit(status);
-
-        } catch (Exception e) {
-
-            transactionManager.rollback(status);
-
-            throw new IllegalStateException(e);
-        }
-
-
+    public void accountTransfer(String fromId, String toId, int money)  {
+        txTemplate.executeWithoutResult((status) -> {
+            try {
+                bizLogic( fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
 
-        Member toMember = memberRepository.findById( toId);
+        Member toMember = memberRepository.findById(toId);
 
         memberRepository.update(fromId, fromMember.getMoney() - money);
         validation(toMember);
